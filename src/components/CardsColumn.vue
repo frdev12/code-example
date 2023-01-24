d2c5a44798ecbce0192ec4d89577da1cf17f023f
Sergey Smirnov <smirnof.sergey@gmail.com>
2023-01-24 19:37:43 +0300

    header

---
+++ src/components/CardsColumn.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'CardsColumn',
+};
+</script>
+
+<template>
+  <div class="cards-column">
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.cards-column {
+  background-color: #EBECF0;
+  width: 200px;
+  height: 200px;
+}
+</style>
---
+++ src/components/UserButton.vue
@@ -0,0 +1,45 @@
+<template>
+  <button class="user-button" :class="{'user-button--in-menu': isInMenu}">
+    {{ userName.charAt(0).toUpperCase() }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'UserButton',
+  props: {
+    userName: {
+      type: String,
+      required: true,
+    },
+    isInMenu: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.user-button {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background-color: #5243AA;
+  font-size: 18px;
+  font-weight: 500;
+  color: white;
+  &:hover {
+    outline: 3px solid #55A9DC;
+    cursor: pointer;
+  }
+  &--in-menu {
+    width: 40px;
+    height: 40px;
+    &:hover {
+      outline: none;
+      cursor: auto;
+    }
+  }
+}
+</style>
